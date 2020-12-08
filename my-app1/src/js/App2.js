084ac79f53c5af5c769acd8ba3b4200eff6b0da5
YodoFaither <xdqapple1356@163.com>
2020-12-08 12:42:34 +0800

    Optimize project structure

---
+++ my-app1/src/js/App2.js
@@ -0,0 +1,31 @@
+import logo2 from '../static/img/logo2.svg';
+import '../css/App2.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo2} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <br/>
+        <table>
+          <th>f</th>
+          <tr>aa</tr>
+          <tr>bbb</tr>
+        </table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
