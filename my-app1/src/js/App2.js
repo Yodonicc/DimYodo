316fd53fb533d7e10c2a9cde887a66217312a6ff
YodoFaither <xdqapple1356@163.com>
2020-12-10 11:08:32 +0800

    sync

--- my-app1/src/js/App2.js
+++ my-app1/src/js/App2.js
@@ -3,8 +3,8 @@ import '../css/App2.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" style={{background : 'rgb(25, 66, 192)'}}>
+      {/* <header className="App-header">
         <img src={logo2} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,7 +23,25 @@ function App() {
           <tr>aa</tr>
           <tr>bbb</tr>
         </table>
-      </header>
+      </header> */}
+      <div id='navbar'>
+        <img id='logo' src="https://goss.veer.com/creative/vcg/veer/800water/veer-375153397.jpg" alt="Girl in a jacket" ></img>
+        <div id='webnameflexbox'>
+          <b id='elementorb'>Elementor</b>
+          <span id='elementorspan'>qa@elementor.io</span>
+        </div>
+        <div id='websitemenu'></div>
+        <link id='searchicon'></link>
+        <input id='searchinput' placeholder='Search for people,document,goods...'></input>
+        <link id='notificationicon'></link>
+        <link id='usericon1'></link>
+        <link id='usericon2'></link>
+        <div id='usermenu'></div>
+      </div>
+      <div id='leftbar'></div>
+      <div id='content'></div>
+      <div id='rightbar'></div>
+      
     </div>
   );
 }
